85f617d1d0d8ee265070c49027a8b14471829e36
yeo myoung Kim <kdjwand@gmail.com>
2025-12-29 17:57:40 +0900

    Update ApplicaionClass

--- DirectX11_Practice/ApplicationClass.h
+++ DirectX11_Practice/ApplicationClass.h
@@ -1,15 +1,5 @@
 #pragma once
-#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
 #include <windows.h>
-#include "D3DClass.h"
-#include "CameraClass.h"
-#include "ModelClass.h"
-#include "ColorShaderClass.h"
-#include "TextureShaderClass.h"
-#include "LightClass.h"
-#include "LightShaderClass.h"
-#include "InputClass.h"
-
 
 const bool FULL_SCREEN = false;
 const bool VSYNC_ENABLED = true;
@@ -29,17 +19,4 @@ public:
 
 private:
 	bool Render(float);
-
-private:
-
-	//  D3DClass의 포인터 추가, 이 튜토리얼에서 모든 멤버 변수는 m_접두사를 사용.
-	D3DClass* m_Direct3D;
-	CameraClass* m_Camera;
-	ModelClass* m_Model;
-	ColorShaderClass* m_ColorShader;
-	TextureShaderClass* m_TextureShader;
-	LightShaderClass* m_LightShader;
-	LightClass* m_Light;
-
-	InputClass* m_Input;
 };
